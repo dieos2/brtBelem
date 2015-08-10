a4cfdc8041ecad624d0ed4f157ef99ad8fc59782
dieos2 <dieos2@gmail.com>
2015-08-10 15:22:28 -0300

    Partls Colocadas

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,20 +1,40 @@
-﻿$.get("_header.html", function (data) {
-    $("#header").html(data);
-
-});
+﻿
 $.get("_menu.html", function (data) {
     $("#menu").html(data);
 
 });
-$.get("_topo.html", function (data) {
-    $("#topo").html(data);
+$.get("_obrt.html", function (data) {
+    $("#o_brt").html(data);
 
 });
-$.get("_menuLateral.html", function (data) {
-    $("#menuLateral").html(data);
+
+$.get("_timeline.html", function (data) {
+    $("#timeline").html(data);
 
 });
-$.get("_rodape.html", function (data) {
-    $("#rodape").html(data);
+$.get("_head.html", function (data) {
+    $("head").append(data);
 
 });
+
+$.get("template.html", function () {
+    $("template").html(data);
+});
+
+
+  var _gaq = _gaq || [];
+var pluginUrl = 
+ '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+
+_gaq.push(['_setAccount', 'UA-49234394-1']);
+_gaq.push(['_setDomainName', 'agenciabelem.com.br']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    /*ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';*/
+    /* ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js'; */
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
