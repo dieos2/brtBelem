78b653685baa596a54112cf53f967746b29701ef
dieos2 <dieos2@gmail.com>
2015-08-10 14:09:44 -0300

    Scipts e Css
    para colocar a API e carregar partils por jquery

---
+++ Scripts/script.js
@@ -0,0 +1,20 @@
+﻿$.get("_header.html", function (data) {
+    $("#header").html(data);
+
+});
+$.get("_menu.html", function (data) {
+    $("#menu").html(data);
+
+});
+$.get("_topo.html", function (data) {
+    $("#topo").html(data);
+
+});
+$.get("_menuLateral.html", function (data) {
+    $("#menuLateral").html(data);
+
+});
+$.get("_rodape.html", function (data) {
+    $("#rodape").html(data);
+
+});
